f5229d427f099ff3f865b6ea475bb6bff31553b0
andreimelo <meloandrei69@gmail.com>
2020-10-11 21:36:18 +0800

    docs:Typo error and change text

--- src/library/common/constants/strings.js
+++ src/library/common/constants/strings.js
@@ -2,7 +2,7 @@
 export const STRINGS = {
 	 TITLE :'Andrei Melo - Personal Website',
 	 NAVIGATION_DATA:["Home",'About me'],
-	 INTRO1:"Hi, I'm Andrei Melo, a dedicated Front-end developer from Philippines and graduated at De La Salle University – Dasmariñas with the degree of Bachelor of Science in Information Technology, i love to build website from modern web technologies. Specifically focusing on ReactJS as of the main time and with minimal skills in designing.",
+	 INTRO1:"Hi, I'm Andrei Melo, a dedicated Front-end developer from Philippines and graduated at De La Salle University – Dasmariñas with the degree of Bachelor of Science in Information Technology, I love to build website from modern web technologies. Specifically focusing on ReactJS as of the mean time and with minimal skills in designing.",
 	 INTRO2:"I love coding, I am a person who loves to spend time coding, reading, prototyping, or learning new things that can improve quality of my development.",
 	 TYPE_DATA:['A FRONT-END DEVELOPER','A JAVASCRIPT DEVELOPER','WELCOME TO MY WEBSITE','CONTACT ME IF YOU WANT TO COLLAB! :x:x'],
 	 BUILT_WITH_NEXTJS:"Built with nextjs",
