5c051eb701f0bbb1b9f6710e709b2c1a3f867afc
andreimelo <andreiagno.melo@gmail.com>
2024-02-13 14:07:38 -0600

    refactor: Fix the issue in ci and replace the resume text to icon

--- src/library/common/components/Button/index.js
+++ src/library/common/components/Button/index.js
@@ -1,17 +1,21 @@
 import PropTypes from 'prop-types';
 
-function Button({ variant, title, onClick }){
+function Button({ variant, title, onClick, imgClass, imgSrc, altImage }){
 	return (
 		<button className={variant} onClick={onClick}>
 			{title}
+			{imgSrc && <img className={imgClass} src={imgSrc} alt={altImage} />}
 		</button>
 	);
 }
 
 Button.propTypes = {
-	variant : PropTypes.string,
-	title   : PropTypes.string,
-	onClick : PropTypes.func.isRequired,
+	variant  : PropTypes.string,
+	title    : PropTypes.string,
+	onClick  : PropTypes.func.isRequired,
+	imgClass : PropTypes.string,
+	imgSrc   : PropTypes.string,
+	altImage : PropTypes.string,
 };
 
 export default React.memo(Button);
