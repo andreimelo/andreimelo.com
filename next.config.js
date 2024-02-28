91619375f5e9cee2ec668528593ebcd85d012fb2
andreimelo <andreiagno.melo@gmail.com>
2024-02-28 02:03:47 -0600

    chore: Change the distDir to build

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ module.exports = {
 	output        : {
 		directory : 'out',
 	},
-	distDir       : 'out',
+	distDir       : 'build',
 	webpack       : function(config){
 		config.module.rules.push({
 			test : /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)$/,
