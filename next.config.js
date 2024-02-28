31d83d4b9329c155124977381b204ebb2b0cb159
andreimelo <andreiagno.melo@gmail.com>
2024-02-28 01:55:58 -0600

    Updated the next config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ module.exports = {
 	output        : {
 		directory : 'out',
 	},
+	distDir       : 'out',
 	webpack       : function(config){
 		config.module.rules.push({
 			test : /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)$/,
