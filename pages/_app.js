9cbd5405bce47e8b77f438e489a73af7f425df6e
andreimelo <andreiagno.melo@gmail.com>
2024-07-21 23:32:23 -0500

    feat: Implement framer-motion on scroll linked progress bar

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,17 @@
 import 'styles/globals.css';
+import { motion, useScroll } from 'framer-motion';
 import LayoutProvider from 'src/library/common/components/LayoutProvider';
 import Navigation from 'src/library/common/components/Navigation';
+import Footer from 'src/library/common/components/Footer';
 import { IMAGES } from 'src/library/common/constants/images';
 import { STRINGS } from 'src/library/common/constants/strings';
-import Footer from 'src/library/common/components/Footer';
+import { progressBar } from 'styles/pages_style/framer.module.css';
 
 function MyApp({ Component, pageProps }){
+	const { scrollYProgress } = useScroll();
 	return (
 		<LayoutProvider>
+			<motion.div className={progressBar} style={{ scaleX: scrollYProgress }} />
 			<Navigation logo={IMAGES.MY_LOGO} data={STRINGS.NAVIGATION_DATA} />
 			<Component {...pageProps} />
 			<Footer />
