19d7cfe1aab6fdaf3171badfb7b3b5a34d3fc4b2
andreimelo <andreiagno.melo@gmail.com>
2024-07-30 22:50:35 -0500

    refactor: Update the blog section, change the date format and footer style

--- pages/posts/components/PostCard/index.js
+++ pages/posts/components/PostCard/index.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import Link from 'next/link';
-import ContentfulImage from 'pages/posts/components/ContentfulImage';
+// import ContentfulImage from 'pages/posts/components/ContentfulImage';
 import DateComponent from '../Date';
-import { post_container, container } from 'styles/components_style/postcard.module.css';
+import {
+	post_container,
+	title_container,
+	default_date,
+	details,
+} from 'styles/components_style/postcard.module.css';
 
 const PostCard = ({ keyPost, post }) => {
 	if (!post) return;
-	const { title, slug, excerpt, picture, date } = post.fields;
-	const image = picture;
+	const { title, slug, excerpt, date } = post.fields;
+	// const image = picture;
 	return (
 		<div key={keyPost} className={post_container}>
 			<Link href={`/posts/${slug}`}>
 				<div>
-					<ContentfulImage
+					{/* <ContentfulImage
 						alt={`Picture for ${title}`}
 						src={image.fields.file.url}
 						width={image.fields.file.details.image.width}
@@ -20,12 +25,12 @@ const PostCard = ({ keyPost, post }) => {
 						placeholder='blur'
 						layout='responsive'
 						blurDataURL={image.fields.file.url}
-					/>
-					<div className={container}>
-						<h3>{title}</h3>
-						<DateComponent dateString={date} />
+					/> */}
+					<div className={title_container}>{title}</div>
+					<div>
+						<DateComponent className={default_date} dateString={date} />
+						<div className={details}>{excerpt}</div>
 					</div>
-					<div>{excerpt}</div>
 				</div>
 			</Link>
 		</div>
--- src/library/utils/format.js
+++ src/library/utils/format.js
@@ -1,4 +1,7 @@
-export function formatDate(dateString, options){
+export function formatDate(
+	dateString,
+	options = { year: 'numeric', month: 'long', day: 'numeric' },
+){
 	const { format } = new Intl.DateTimeFormat('en-US', options);
 	return format(new Date(dateString));
 }
