bc21730abffad0ac94a441a0697fb375eb34da09
andreimelo <andreiagno.melo@gmail.com>
2024-08-02 23:17:59 -0500

    refactor: Update the blog header, post and body and styles

--- pages/posts/components/ContentfulImage/index.js
+++ pages/posts/components/ContentfulImage/index.js
@@ -1,11 +1,16 @@
 /* eslint-disable jsx-ally/alt-text */
-import Image from 'next/image';
 
-const contentfulLoader = ({ src, width, quality }) => {
-	return `${src}?w=${width}&q=${quality || 75}`;
-};
+// const contentfulLoader = ({ src, width, quality }) => {
+// 	return `${src}?w=${width}&q=${quality || 75}`;
+// };
+
 const ContentfulImage = (props) => {
-	return <Image loader={contentfulLoader} {...props} />;
+	return (
+		<img
+			// loader={contentfulLoader}
+			{...props}
+		/>
+	);
 };
 
 export default ContentfulImage;
